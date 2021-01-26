cf4eda71f502b4afadefc6d4bf92d8560a7c4ea2
Willian Silva <will.devfrontend@gmail.com>
2021-01-26 19:21:16 -0300

    feat: create GenerationsPokemons.vue component

---
+++ src/components/Home/GenerationsPokemons.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Pokemons</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GenerationsPokemons',
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
