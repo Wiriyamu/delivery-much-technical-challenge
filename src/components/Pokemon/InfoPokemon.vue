6c4624587e6e90d6dd12c66224df92aded688b2c
Willian Silva <will.devfrontend@gmail.com>
2021-01-27 11:47:53 -0300

    feat: create component InfoPokemon.vue

---
+++ src/components/Pokemon/InfoPokemon.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Pokemon
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InfoPokemon',
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
