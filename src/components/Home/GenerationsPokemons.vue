4a7e341ee744e4ea8ddfa1d4eae89d35c681d02c
Willian Silva <will.devfrontend@gmail.com>
2021-01-26 22:43:28 -0300

    feat: add pokemons images

--- src/components/Home/GenerationsPokemons.vue
+++ src/components/Home/GenerationsPokemons.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
-    <h1>Pokemons</h1>
-  </div>
+  <section class="generation">
+    <div class="generation__container">
+      <header class="generation__header">
+        <img src="../../assets/pokeApiLogo.svg" alt="pokemon api logo" />
+      </header>
+
+      <h1 class="generation__description">
+        Descubra tudo sobre as gerações de pokemóns!
+      </h1>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -13,4 +21,26 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.generation {
+  width: 100%;
+
+  &__container {
+    width: 100%;
+    max-width: 998px;
+    margin: 0 auto;
+    padding: 0 2%;
+  }
+
+  &__header {
+    display: flex;
+    justify-content: center;
+    margin-top: 2rem;
+  }
+
+  &__description {
+    margin-top: 2rem;
+    text-align: center;
+  }
+}
+</style>
