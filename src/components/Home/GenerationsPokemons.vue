e7c0c1aa657227019647c43a219901004fdc8355
Willian Silva <will.devfrontend@gmail.com>
2021-01-26 22:46:16 -0300

    feat(generations-pokemons.vue): add base styles

--- src/components/Home/GenerationsPokemons.vue
+++ src/components/Home/GenerationsPokemons.vue
@@ -8,6 +8,72 @@
       <h1 class="generation__description">
         Descubra tudo sobre as gerações de pokemóns!
       </h1>
+
+      <section class="generation__cardsContainer">
+        <div class="generation__cards">
+          <h2 class="generation__cardName">1° Geração</h2>
+          <figure class="generation__pokemon">
+            <img src="../../assets/pokemonGeracao1.png" />
+          </figure>
+          <button class="generation__button">Saiba Mais</button>
+        </div>
+
+        <div class="generation__cards">
+          <h2 class="generation__cardName">2° Geração</h2>
+          <figure class="generation__pokemon">
+            <img src="../../assets/pokemonGeracao2.png" />
+          </figure>
+          <button class="generation__button">Saiba Mais</button>
+        </div>
+
+        <div class="generation__cards">
+          <h2 class="generation__cardName">3° Geração</h2>
+          <figure class="generation__pokemon">
+            <img src="../../assets/pokemonGeracao3.png" />
+          </figure>
+          <button class="generation__button">Saiba Mais</button>
+        </div>
+
+        <div class="generation__cards">
+          <h2 class="generation__cardName">4° Geração</h2>
+          <figure class="generation__pokemon">
+            <img src="../../assets/pokemonGeracao4.png" />
+          </figure>
+          <button class="generation__button">Saiba Mais</button>
+        </div>
+
+        <div class="generation__cards">
+          <h2 class="generation__cardName">5° Geração</h2>
+          <figure class="generation__pokemon">
+            <img src="../../assets/pokemonGeracao5.png" />
+          </figure>
+          <button class="generation__button">Saiba Mais</button>
+        </div>
+
+        <div class="generation__cards">
+          <h2 class="generation__cardName">6° Geração</h2>
+          <figure class="generation__pokemon">
+            <img src="../../assets/pokemonGeracao6.png" />
+          </figure>
+          <button class="generation__button">Saiba Mais</button>
+        </div>
+
+        <div class="generation__cards">
+          <h2 class="generation__cardName">7° Geração</h2>
+          <figure class="generation__pokemon">
+            <img src="../../assets/pokemonGeracao7.png" />
+          </figure>
+          <button class="generation__button">Saiba Mais</button>
+        </div>
+
+        <div class="generation__cards">
+          <h2 class="generation__cardName">8° Geração</h2>
+          <figure class="generation__pokemon">
+            <img src="../../assets/pokemonGeracao8.png" />
+          </figure>
+          <button class="generation__button">Saiba Mais</button>
+        </div>
+      </section>
     </div>
   </section>
 </template>
@@ -41,6 +107,43 @@ export default {
   &__description {
     margin-top: 2rem;
     text-align: center;
+  }
+
+  &__cardsContainer {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    grid-auto-columns: 300px 300px;
+  }
+
+  &__cards {
+    display: grid;
+    width: 300px;
+    height: 300px;
+    margin: 50px 30px;
+    background-color: #fff;
+  }
+
+  &__cardName {
+    padding-top: 20px;
+    text-align: center;
+  }
+
+  &__pokemon {
+    display: flex;
+    justify-content: center;
+  }
+
+  &__button {
+    padding: 20px 0;
+    color: #fff;
+    cursor: pointer;
+    background-color: #ef5350;
+
+    &:hover {
+      transition: 0.3s ease-in;
+      background-color: #3761a8;
+    }
   }
 }
 </style>
